ecf6344ef9315183656ca0a59e1e44bec4975042
Arachnid02 <jianwen.chen@uamaker.nyc>
2019-02-12 11:10:22 -0500

    Added first shape, red triangle

--- shapes5.py
+++ shapes5.py
@@ -3,5 +3,9 @@ from graphics import *
 window = GraphWin("Window", 1000, 1000)
 window.setCoords(0, 0, 1000, 1000)
 
+triangle = Polygon(Point(50, 50), Point(50, 200), Point(200, 50))
+triangle.setFill(color_rgb(255, 0, 0))
+triangle.draw(window)
+
 window.getMouse()
 window.close()
