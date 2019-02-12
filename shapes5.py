26c4cc036a23ffdb865ada298be25f7f24caedf9
Arachnid02 <jianwen.chen@uamaker.nyc>
2019-02-12 11:11:32 -0500

    added second shape, green square

--- shapes5.py
+++ shapes5.py
@@ -7,5 +7,9 @@ triangle = Polygon(Point(50, 50), Point(50, 200), Point(200, 50))
 triangle.setFill(color_rgb(255, 0, 0))
 triangle.draw(window)
 
+square = Polygon(Point(50, 950), Point(200,950), Point(200, 800), Point(50, 800))
+square.setFill(color_rgb(0, 255, 0))
+square.draw(window)
+
 window.getMouse()
 window.close()
